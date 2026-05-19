aaf6d601cb9e72bf851d92a0b41d736a493cfd52
Віталій Миколайович Петлицький <Vitalii.Petlytskyi@forum-am.in>
2026-05-19 14:58:08 +0300

    commit Auto

--- src/index.jsx
+++ src/index.jsx
@@ -5,3 +5,8 @@ import App from './App';
 const root = document.getElementById('root');
 
 render(() => <App />, root);
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  // зберігаємо подію
+  window._installPrompt = e;
+});
